5395cc502da6d8d31e0e7160b21c7d5ef3b86c56
taiem22 <tranbatai2202@gmail.com>
2022-12-16 11:33:06 +0700

    case

---
+++ case_study/database/furama_query_16_20.sql
@@ -0,0 +1,20 @@
+use furama_resort;
+
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+
+
+-- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum 
+-- lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với 
+-- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+
+
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+
+
+-- 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng
+--  trên 10 lần trong năm 2020 lên gấp đôi.
+
+
+-- 20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng 
+-- có trong hệ thống, thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), 
+-- ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
